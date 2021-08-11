1f150f0e12b201879833258e48340d923561bd44
expresshihway <03wow22@gmail.com>
2021-08-11 14:03:30 +0900

    [update] post_detail 추가

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+
 
 def post_list(request):
   qs = Post.objects.all()
   qs = qs.filter(published_date__lte=timezone.now())
   qs = qs.order_by('published_date')
-  
+
   return render(request, 'blog/post_list.html',{
     'post_list': qs,
+  })
+
+def post_detail(request, pk):
+  # try:
+  #   post = Post.objects.get(pk=pk)
+  # except Post.DoesNotExist:
+  #   raise Http404
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, 'blog/post_detail.html', {
+    'post': post
   })
